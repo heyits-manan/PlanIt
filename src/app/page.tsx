10ea5c31e0d669cc78e367f0343d9479dc6d5c8c
Manan Chataut <mananchataut@gmail.com>
2024-11-24 23:01:05 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <div className="container mx-auto p-4">
-        <h1 className="text-4xl font-bold text-center text-black">
-          Trello Clone
-        </h1>
+        <h1 className="text-4xl font-bold text-center text-black">PlanIt</h1>
         <AddBoard />
         <BoardList />
       </div>
