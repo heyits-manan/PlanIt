8ea0feda59bf1f07c79ce859febc5208464d33b8
Manan Chataut <mananchataut@gmail.com>
2024-11-28 15:10:26 +0530

    improve layout

--- src/app/boards/layout.tsx
+++ src/app/boards/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Boards",
+  title: "Boards | PlanIt",
   description: "Create your boards",
 };
 
